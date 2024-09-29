40ea1f1e9b97041a32c3ea55a55108dc7196e10f
Aryan <106865458+Aryan1396@users.noreply.github.com>
2024-09-29 14:25:34 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://neat-toucan-50.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
